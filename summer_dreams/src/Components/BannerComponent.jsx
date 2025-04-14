86c272e08bf5c75f3569ca4c5e77ff4b8b4b6ec5
BM-ACADEMY <teamkamarweb@gmail.com>
2025-04-14 16:58:43 +0530

    lastt

--- summer_dreams/src/Components/BannerComponent.jsx
+++ summer_dreams/src/Components/BannerComponent.jsx
@@ -9,8 +9,14 @@ import CardImage3 from '../assets/card3.jpeg';
 
 const BannerComponent = () => {
   return (
-    <div className="container-fluid py-5" id="banner">
+    <div className="container2 py-5" id="banner">
+
       <div className="row justify-content-center">
+      <h2 className="mt-4 fs-3 fs-sm-4 fs-xl-5 p-4 fw-bold">
+  
+        <span style={{ color: 'red' }}>Premium Rooms with</span>{' '}
+        <span style={{ color: 'blue' }}>Bolcony Mountain View Point</span>
+      </h2> <br /> <br />
         {/* Card 1 */}
         <div className="col-md-4 col-sm-6 col-12 mb-4">
           <div className="card banner-card">
